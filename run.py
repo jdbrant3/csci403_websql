2d0a5597539036dc1f9f23f88ae7f25223df89e4
Christopher Painter-Wakefield <cpainter@mines.edu>
2020-09-26 15:19:34 -0600

    Some refactoring to pull duplicate code into fetch_cursor()

--- run.py
+++ run.py
@@ -2,20 +2,14 @@ import json
 import re
 from datetime import datetime, timedelta
 from random import *
-
-import pandas as pd
-import pandas.io.sql as psql
 import psycopg2 as pg
-import requests
+from pgspecial.main import PGSpecial
 from cryptography.fernet import Fernet
+import requests
 from flask import (
     Flask, jsonify, redirect, render_template, request, session, url_for)
 from flask_cors import CORS, cross_origin
-from psycopg2 import Error
 from do_config import config
-
-from pgspecial.main import PGSpecial
-from pgspecial.namedqueries import NamedQueries
 
 app = Flask(__name__,
             static_folder = "../dist/static",
@@ -29,7 +23,200 @@ config(app)
 
 pgspecial = PGSpecial()
 
+# execute a SQL query and return the results
+@app.route('/api/query', methods=['POST'])
+def execute_query():
+    cursor, fail_message, fail_code = fetch_cursor()
+    if cursor:
+        results = []
+        queries = parse(request.json['query'])
 
+        for (query, comment) in queries:
+            if query == '':
+                if len(comment) > 0:
+                    results.append({ 'comment' : comment })
+            elif query.lower()[0:5] == 'show ' or query.lower()[0:7] == 'select ':
+                query = query + ';'
+                try:
+                    cursor.execute(query)
+                    rowcount = cursor.rowcount
+
+                    # should make this a settable limit on the client side at some point
+                    limit_message = ''
+                    if (rowcount > 500):
+                        data = cursor.fetchmany(500)
+                        limit_message = 'Query resulted in ' + str(rowcount) + ' rows, exceeding limit.  First 500 rows displayed above.'
+                    else:
+                        data = cursor.fetchall()
+
+                    # convert to strings, not all types convert to JSON
+                    data = [ [ str(item or '<null>') for item in row ] for row in data ]
+
+                    columns = [ el[0] for el in cursor.description ]
+
+                    results.append({ 'query' : query,
+                                     'comment' : comment,
+                                     'data' : data ,
+                                     'columns' : columns,
+                                     'limit_message' : limit_message
+                                     })
+                except pg.Error as e:
+                    results.append({ 'query' : query, 'comment' : comment, 'error' : str(e) })
+                    break
+            else:
+                query = query + ';'
+                try:
+                    cursor.execute(query)
+                    rowcount = cursor.rowcount
+                    results.append({ 'query' : query, 'comment' : comment, 'message' : cursor.statusmessage })
+                except pg.Error as e:
+                    results.append({ 'query' : query, 'comment' : comment, 'error' : str(e) })
+                    break
+
+        cursor.close()
+        cursor.connection.close()
+        return json.dumps(results)
+
+    else:
+        app.logger.exception(fail_message)
+        return jsonify({ 'message': fail_message }), fail_code
+
+# Implements \d
+@app.route('/api/describe', methods=['GET'])
+def describe():
+    cursor, fail_message, fail_code = fetch_cursor()
+    if cursor:
+        results = []
+
+        for result in pgspecial.execute(cursor, "\d"):
+            header = result[2]
+        data = cursor.fetchall()
+
+        results.append({ 'data': data, 'columns': header })
+
+        cursor.close()
+        cursor.connection.close()
+        return json.dumps(results)
+
+    else:
+        app.logger.exception(fail_message)
+        return jsonify({ 'message': fail_message }), fail_code
+
+# Implements \d object or \d+ object
+@app.route('/api/describe_object', methods=['POST'])
+def describe_object():
+    cursor, fail_message, fail_code = fetch_cursor()
+    if cursor:
+        results = []
+
+        for result in pgspecial.execute(cursor, "\dt"):
+            header = result[2]
+        data = cursor.fetchall()
+
+        results.append({ 'data': data, 'columns': header })
+
+        cursor.close()
+        cursor.connection.close()
+
+        return json.dumps(results)
+        
+    else:
+        app.logger.exception(fail_message)
+        return jsonify({ 'message': fail_message }), fail_code
+
+# route for user login
+@app.route('/api/login', methods=['POST'])
+def login():
+
+    username = request.json['username'].strip()
+    password = request.json['password'].strip()
+
+    if authorize_login(username, password):
+        
+        # if user is not logged in already then create new session for user
+        if username not in session:
+            byte_pass = password.encode()
+            f = Fernet(app.secret_key)
+            encrypt_pass = f.encrypt(byte_pass)
+            session['username'] = username
+            session['password'] = encrypt_pass
+
+            return jsonify({'username': username, 'authorized': True}), 200
+        else:
+            return jsonify({'username': username, 'authorized': True}), 200
+    # If user not authenticated then return a flask authorization
+    else:
+        return jsonify({'username': username, 'authorized': False}), 200
+
+
+# Remove user credentials from session object
+@app.route("/api/logout", methods=["POST"])
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return jsonify({'success': True})
+
+# test for existence of user credentials
+@app.route("/api/is_logged_in")
+def is_logged_in():
+    if 'username' in session:
+        return jsonify({ 'loggedIn': True })
+    else:
+        return jsonify({ 'loggedIn': False })
+
+# default route returns static index.html
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
+
+
+# If connecting to database is successful then credentials are authenticated
+def authorize_login(username, password):
+    connection = fetch_connection(username, password)
+    if connection:
+        connection = None
+        return True
+    else:
+        return False
+
+# Get a database connection for a given username and password
+def fetch_connection(username, password):
+    try:
+        connection = pg.connect(user = username,
+                                password = password,
+                                host = app.config['host'],
+                                port = app.config['port'],
+                                database = app.config['dbname'])
+
+        connection.autocommit = True
+        return connection
+    except (Exception, pg.error) as error:
+        app.logger.error(error)
+        return None
+
+# Connect to the database and obtain a database cursor, using the credentials
+# of the currently logged in user
+def fetch_cursor():
+    try:
+        if 'username' not in session:
+            return None, 'Not logged in', 401
+        else:
+            session_username = session['username']
+            f = Fernet(app.secret_key)
+            session_password = f.decrypt(session['password']).decode('utf-8')
+
+            connection = fetch_connection(session_username, session_password)
+            cursor = connection.cursor()
+            return cursor, None, None
+    except (Exception, pg.Error) as error :
+        app.logger.exception(error)
+        return None, 'Connection failed', 401
+
+# Parse query string, separating out individual SQL statements, and comments where
+# possible.
 def parse(query_string):
     # comments can occur both outside and inside queries; and semicolons can occur
     # inside comments.  This makes parsing tricky, if we want to retain comments
@@ -89,205 +276,3 @@ def parse(query_string):
             break
 
     return result
-
-
-@app.route('/api/query', methods=['POST'])
-def execute_query():
-    try:
-
-        if 'username' not in session:
-            return jsonify({'message': 'Not logged in'})
-
-        else:
-            session_username = session['username']
-            f = Fernet(app.secret_key)
-            session_password = f.decrypt(session['password']).decode('utf-8')
-
-            connection = fetch_connection(session_username, session_password)
-            cursor = connection.cursor()
-
-            queries = parse(request.json['query'])
-            results = []
-
-        for (query, comment) in queries:
-            if query == '':
-                if len(comment) > 0:
-                    results.append({ 'comment' : comment })
-            elif query.lower()[0:5] == 'show ' or query.lower()[0:7] == 'select ':
-                query = query + ';'
-                try:
-                    cursor.execute(query)
-                    rowcount = cursor.rowcount
-
-                    # should make this a settable limit on the client side at some point
-                    limit_message = ''
-                    if (rowcount > 500):
-                        data = cursor.fetchmany(500)
-                        limit_message = 'Query resulted in ' + str(rowcount) + ' rows, exceeding limit.  First 500 rows displayed above.'
-                    else:
-                        data = cursor.fetchall()
-
-                    # convert to strings, not all types convert to JSON
-                    data = [ [ str(item) for item in row ] for row in data ]
-
-                    columns = [ el[0] for el in cursor.description ]
-
-                    results.append({ 'query' : query,
-                                     'comment' : comment,
-                                     'data' : data ,
-                                     'columns' : columns,
-                                     'limit_message' : limit_message
-                                     })
-                except pg.Error as e:
-                    results.append({ 'query' : query, 'comment' : comment, 'error' : str(e) })
-                    break
-            else:
-                query = query + ';'
-                try:
-                    cursor.execute(query)
-                    rowcount = cursor.rowcount
-                    results.append({ 'query' : query, 'comment' : comment, 'message' : cursor.statusmessage })
-                except pg.Error as e:
-                    results.append({ 'query' : query, 'comment' : comment, 'error' : str(e) })
-                    break
-
-        cursor.close()
-        connection.close()
-        return json.dumps(results)
-
-    except (Exception, pg.Error) as error :
-        app.logger.exception(error)
-        return jsonify({ 'message': 'Connection Failed' }), 401
-
-
-@app.route('/api/describe', methods=['POST', 'GET'])
-def describe():
-    try:
-
-        if 'username' not in session:
-            return jsonify({'message': 'Not logged in'}), 400
-
-        else:
-            session_username = session['username']
-            f = Fernet(app.secret_key)
-            session_password = f.decrypt(session['password']).decode('utf-8')
-
-            connection = fetch_connection(session_username, session_password)
-            cursor = connection.cursor()
-
-            results = []
-
-        for result in pgspecial.execute(cursor, "\d"):
-            header = result[2]
-        data = cursor.fetchall()
-
-        results.append({ 'data': data, 'columns': header })
-
-        cursor.close()
-        connection.close()
-        return json.dumps(results)
-
-    except (Exception, pg.Error) as error :
-        app.logger.exception(error)
-        return jsonify({ 'message': 'Connection Failed' }), 401
-
-
-@app.route('/api/describe_object', methods=['POST', 'GET'])
-def describe_object():
-    try:
-
-        session_username = session['username']
-        f = Fernet(app.secret_key)
-        session_password = f.decrypt(session['password']).decode('utf-8')
-
-        connection = fetch_connection(session_username, session_password)
-        cursor = connection.cursor()
-
-        results = []
-
-        for result in pgspecial.execute(cursor, "\dt"):
-            header = result[2]
-        data = cursor.fetchall()
-
-        results.append({ 'data': data, 'columns': header })
-
-        cursor.close()
-        connection.close()
-        return json.dumps(results)
-        
-    except (Exception, pg.Error) as error :
-        app.logger.exception(error)
-        return jsonify({ 'message': 'Connection Failed' }), 401
-
-
-@app.route('/api/login', methods=['POST'])
-def login():
-
-    username = request.json['username'].strip()
-    password = request.json['password'].strip()
-
-    if authorize_login(username, password):
-        
-        # if user is not logged in already then create new session for user
-        if username not in session:
-            byte_pass = password.encode()
-            f = Fernet(app.secret_key)
-            encrypt_pass = f.encrypt(byte_pass)
-            session['username'] = username
-            session['password'] = encrypt_pass
-
-            return jsonify({'username': username, 'authorized': True}), 200
-        else:
-            return jsonify({'username': username, 'authorized': True}), 200
-    # If user not authenticated then return a flase authorization
-    else:
-        return jsonify({'username': username, 'authorized': False}), 200
-
-# Remove user credentials from session object
-@app.route("/api/logout", methods=["POST"])
-def logout():
-    session.pop('username', None)
-    session.pop('password', None)
-    return jsonify({'success': True})
-
-
-# If connecting to database is successful then credentials are authenticated
-def authorize_login(username, password):
-
-    connection = fetch_connection(username, password)
-    if connection:
-        connection = None
-        return True
-    else:
-        return False
-
-
-def fetch_connection(username, password):
-    try:
-        connection = pg.connect(user = username,
-                                password = password,
-                                host = app.config['host'],
-                                port = app.config['port'],
-                                database = app.config['dbname'])
-
-        connection.autocommit = True
-        return connection
-    except (Exception, pg.error) as error:
-        app.logger.error(error)
-        return None
-
-
-@app.route("/api/is_logged_in")
-def is_logged_in():
-    if 'username' in session:
-        return jsonify({ 'loggedIn': True })
-    else:
-        return jsonify({ 'loggedIn': False })
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    if app.debug:
-        return requests.get('http://localhost:8080/{}'.format(path)).text
-    return render_template("index.html")
